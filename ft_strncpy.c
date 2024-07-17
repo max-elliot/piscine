8d193a688c580a389a981be5df45c93d5977ebcb
max-elliot <167291658+max-elliot@users.noreply.github.com>
2024-07-17 19:04:58 +0200

    Add files via upload

---
+++ ft_strncpy.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char 	*ft_strcpy(char *dest, char *src, unsigned int n)
+{	
+	unsigned int i = 0;
+
+	while(i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++; 
+	}
+	return (dest);
+}
+
+int main()
+{
+	char source[] = "Hello World!";
+	char destination[20];
+	
+	ft_strcpy(destination, source, 5);
+	printf("After 5 char: %s\n", destination);
+	
+	ft_strcpy(destination, source, 20);
+	printf("After 20 char: %s\n", destination);
+	return (0);
+}
